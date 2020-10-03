489bbcbaea5316ec8efe85d65d30238b8a4b3b6b
OliverTuesta <oliver.jtuesta@gmail.com>
2020-10-02 20:47:24 -0500

    Funciones View y Make creadas

--- src/RecipesMaker.kt
+++ src/RecipesMaker.kt
@@ -1,6 +1,10 @@
+import java.lang.NullPointerException
+
+val numberSelected={ readLine()?.toInt()}
+
 fun main(args: Array<String>){
 
-    salir@do {
+    do {
         println(
             """
         Selecciona la opción deseada
@@ -9,13 +13,34 @@ fun main(args: Array<String>){
     3. Salir        
     """.trimIndent()
         )
-        val selected = readLine()
+        val selected = numberSelected()
 
         when (selected) {
-            "1" -> println("Seleccionaste Hacer una Receta")
-            "2" -> println("Seleccionaste ver mis Recetas")
-            "3" -> println("Gracias por visitarnos")
+            1 -> makeRecipe()
+            2 ->  println("Seleccionaste ver mis Recetas")
+            3 -> println("Gracias por visitarnos")
             else -> println("Escoge una de las opciones")
         }
-    }while (!selected.equals("3"))
+    }while (selected!=3)
 }
+
+fun makeRecipe(){
+    do {
+        println("""
+        Hacer receta
+        Selecciona por categoría el ingrediente que buscas
+    1. Agua
+    2. Leche
+    3. Carne
+    4. Verduras
+    5. Frutas
+    6. Cereal
+    7. Huevos
+    8. Aceites""".trimIndent())
+        val selected = numberSelected()
+    }while (!(selected in (1..8)))
+}
+
+fun viewRecipe(){println("""
+        Ver mis recetas
+""".trimIndent())}
